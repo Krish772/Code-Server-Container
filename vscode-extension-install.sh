83a5392a73ca0f64587141aca8230f063edbc7dd
Krish772 <87887895+Krish772@users.noreply.github.com>
2022-05-09 21:31:43 +0530

    Update vscode-extension-install.sh

--- vscode-extension-install.sh
+++ vscode-extension-install.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-
-cat /tmp/extensions.txt | while read extension || [[ -n $extension ]]
+cat extensions.txt | while read extension || [[ -n $extension ]];
+do
+  code --install-extension $extension --force
+done
